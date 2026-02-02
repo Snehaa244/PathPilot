91e31eab761106615fa32a0e17271d01214bcb8d
Sneha Parmar <parmarsneha789@gmail.com>
2026-02-02 22:54:07 +0530

    use controller updated

--- pathpilot-backend/controllers/userController.js
+++ pathpilot-backend/controllers/userController.js
@@ -1,8 +1,44 @@
-const User = require('../models/User');
+const User = require("../models/User.model");
 
-exports.getProfile = async(req,res)=>{
-    try{
-   const user = await User.findOne(req.user.id)
-   
+/**
+ * GET /api/user/me
+ * Get logged-in user profile
+ */
+const getMyProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId).select("-password -refreshToken");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * PUT /api/user/me
+ * Update profile
+ */
+const updateMyProfile = async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      req.user.userId,
+      { name },
+      { new: true }
+    ).select("-password -refreshToken");
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getMyProfile,
+  updateMyProfile,
+};
