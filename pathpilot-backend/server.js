05f794660397c3e0ad05de98bf40f51859f85981
Sneha Parmar <parmarsneha789@gmail.com>
2026-02-02 23:02:09 +0530

    server updates

--- pathpilot-backend/server.js
+++ pathpilot-backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
